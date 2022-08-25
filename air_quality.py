d53b53d2eff22b4c108923f66e2d870a00f6e9f9
Chris Dedman-Rollet <61106361+chrisdedman@users.noreply.github.com>
2022-08-25 13:29:55 -0700

    Majors update -> Data Structure

--- air_quality.py
+++ air_quality.py
@@ -1,139 +1,142 @@
-# The program will provide different Air Quality information base on a zip code. 
-# The informations are:
-# The location of the monitoring station
-# The dominant pollutant
-# The current AQI(air quality index)
-# Ozone, PM10, PM2.5, UV Index information for the current day, the previous two days, and a 5 day forecast
-# Information includes average, min, and max
-# Plus, category that explain the informations about the index scales.
-
-'''Created by Chris Dedman-Rollet, 12/06/2021'''
-
-# Import packtage needed
-from geopy.geocoders import Nominatim       # <-- Geolocator to make the API working
-from datetime import datetime               # <-- Date & Time to import the current time in the user location
-import requests                             # <-- Request library to get the API URL Working
-import json                                 # <-- Json library to extract data from json file
-import time                                 # <-- Controle the time sleep in the program
-import sys                                  # <-- Library use to exit the program when the user is done
-import os                                   # <-- Library use to clear the terminal when move to different categories
+from collections import defaultdict        # <-- Structure the data into dictionary
+from geopy.geocoders import Nominatim      # <-- Geolocator to make the API working
+from datetime import datetime              # <-- Date & Time to import the current time in the user location
+from requests import get                   # <-- Request library to get the API URL Working
+from time import sleep                     # <-- Controle the time sleep in the program
+from sys import exit                       # <-- Library use to exit the program when the user is done
+from os import system                      # <-- Library use to clear the terminal
+from timezonefinder import TimezoneFinder
+from pytz import timezone
 
 
-# Extract weather informations from World Air Quality Index website, and store the datas into variables in json format
-def data():
-    # Global variable to be export the variable into different functions
-    global pm_ten, pm_twofive, ozone, uvi, current_time, pollutant, aqi, current_pm25, current_city
+def get_api_data(BASE_URL, location, key_api):
+    """Get the Air Quality API Data"""
+
+    api = get(f"{BASE_URL}{location}/?token={key_api}");
+    if api.status_code == 200:
+        return api.json();
+    return None;
+
+def get_timezone(zip_code):
+    """Get the current time zone according to zip code"""
+
+    geolocator = Nominatim(user_agent="MyGeoPy");
+    location   = geolocator.geocode({'postalcode': int(zip_code)}) if zip_code.isnumeric() else 0;
+    timezone_finder = TimezoneFinder();
+
+    result = timezone_finder.timezone_at(
+        lng=location.longitude,
+        lat=location.latitude);
+        
+    return datetime.now(timezone(result));
+
+def get_location(zip_code):
+    """Get the user location base on zip code"""
     
-    while True:
-        geolocator = Nominatim(user_agent="MyGeoPy")
-        keyAPI = "ENTER YOUR OWN TOKEM HERE"     # <-- TOKEN to make the API work from the website on the next line (waqi.info)
-        url = "https://api.waqi.info/feed/geo:"
-        # the user enter a zip code
-        addr = input("Please, provide your zip code: ")
+    try:
+        geolocator = Nominatim(user_agent="MyGeoPy");
+        postal     = geolocator.geocode({'postalcode': int(zip_code)}) if zip_code.isnumeric() else 0;
+        location   = f"{postal.latitude};{postal.longitude}";
 
-        # if the user entering a correct zip number, proceed gathering informations
-        if (addr.isnumeric()):
-            # Handle invalid zip code
-            try:
-                # geopy translate the zip code into cardinal point
-                postal = geolocator.geocode({'postalcode': int(addr)})
-                location = f"{postal.latitude};{postal.longitude}"
-                # complete url for the API to proccess data
-                complete_url = f"{url}{location}{keyAPI}"
-                # the API get the data from the url
-                response = requests.get(complete_url)
-                x = response.json()  # the data are translate and store into the x variable in json format
+    except:
+        print("Sorry, based on your input, we were unable to collect any information from our database ...")
+        exit(0)
 
-                y = x['data']  # extract the json data and store it into a variable
-                aqi = y['aqi']  # current Air Quality Index
-                current_pm25 = y['iaqi']['pm25']['v']  # current PM2.5 index
-                pollutant = y['dominentpol']  # current dominent pollutant
-                current_city = y['city']['name']  # name of the current city
-                current_time = y['time']['s'][:10]  # current_time[:9]
-                ozone = y['forecast']['daily']['o3']  # prediction o3
-                pm_ten = y['forecast']['daily']['pm10']  # prediction PM10
-                pm_twofive = y['forecast']['daily']['pm25']  # prediction PM2.5
-                uvi = y['forecast']['daily']['uvi']  # prediction UV index
+    return location;
 
-                # break the while loop once the data are gathered, and move to the next function
-                break
+def get_air_quality_info(time_zone, data):
+    """Get Air Quality Informations Stored into Dictionary"""
 
-            # If zip code no found, print an error and start over.
-            except (AttributeError, KeyError):
-                print("Sorry, based on your input, we were unable to collect any information from our database ... \nPlease enter another zip code! \n")
-                continue            
+    air_quality_info = defaultdict(list);
+    current_time     = time_zone.strftime("%Y-%m-%d %H:%M");
+    forecast         = get_forecast(data);
 
-        # If the user do not enter a numerical zip code, print an error and start again.
-        else:
-            print('Invalid Input!\n')
-            continue
+    air_quality_info['current_aqi']        = data['aqi'];
+    air_quality_info['current_pm25']       = data['iaqi']['pm25']['v'];
+    air_quality_info['dominant_pollutant'] = data['dominentpol'];
+    air_quality_info['current_city']       = data['city']['name'];
+    air_quality_info['current_time']       = current_time;
+    air_quality_info['ozone']              = forecast['o3'];
+    air_quality_info['pm_ten']             = forecast['pm10'];
+    air_quality_info['pm_twofive']         = forecast['pm25'];
+    air_quality_info['uvi']                = forecast['uvi'];
 
-# user menu to access different air quality informations
-def air():
-    os.system("cls||clear")
-    print("""
-    \t\t\t ________________________
-    \t\t\t|Welcome to AirQualityPy!|
-    """)
-    # Gathered the current time
-    now = datetime.now()
-    time_now = now.strftime("%H:%M")
+    return air_quality_info;
 
-    # Print the user menu with different category option.
-    print(f"\n\tCurrent Date & Time: {current_time} / {time_now}\n\tDominant pollutant: {pollutant}\n\tCurrent Air Quality Index: {aqi}\n\tCurrent PM2.5 value: {current_pm25}\n\tStation Location: {current_city}\n")
-    print(
-        "\t\t1.[PM10] / 2.[PM2.5] / 3.[Ozone] / 4.[UV Index]\n\t5.[Index Level Scale] / 6.[Data Reference information] / 7.[Quit]")
+def get_forecast(data):
+    """Get Forecast informations"""
 
-    # Main user navigation between categories
-    request = input("\nWhich information do you want to access? [Index #]\n>>")
+    return data['forecast']['daily'];
 
-    # IF the user input a non digital number, print a warning and try again. Else, proceed to the category
-    if not (request.isnumeric()):
-        print("Digit Number Only!")
-        time.sleep(1)
+def get_forecast_info(forecast_element):
+    """Get forecast for each Air Quality Element"""
+
+    element = defaultdict(list)
+
+    for forecast in range(len(forecast_element)):
+        element['day']     += [forecast_element[forecast]['day']];
+        element['average'] += [forecast_element[forecast]['avg']];
+        element['max']     += [forecast_element[forecast]['max']];
+        element['min']     += [forecast_element[forecast]['min']];
+
+    return element;
+
+def display_air_quality_element(aq_info):
+    """Display Air Quality Element Informations"""
+
+    clear_console();
+    for information in range(len(aq_info)):
+        print(f"""
+        Forecast Date -> {aq_info[information]['day']}
+        Average Index -> {aq_info[information]['avg']}
+        Max Index     -> {aq_info[information]['max']}
+        Min Index     -> {aq_info[information]['min']}
+        """)
+
+    print("Press enter to return to air quality information!")
+    input()
+
+def fetch_forecast_air_quality(menu_option, air_quality_info):
+    """Fetch Air Quality Forecast Informations"""
+    
+    if not (menu_option.isnumeric()):
+        print("Enter Digit Only!")
+        sleep(1)
 
     else:
-        # convert the request variable into integer
-        request = int(request)
+        menu_option = int(menu_option)
 
-        # If the user chose the index number 1, move to the PM10 category
-        if request == 1:
-            pmten()
+        if menu_option == 1:
+            return air_quality_info['pm_ten'];
         
-        # If the user chose the index number 2, move to the PM2.5 category
-        elif request == 2:
-            pmtwo()
+        elif menu_option == 2:
+            return air_quality_info['pm_twofive'];
 
-        # If the user chose the index number 3, move to the Ozone category
-        elif request == 3:
-            oz()
+        elif menu_option == 3:
+            return air_quality_info['ozone'];
 
-        # If the user chose the index number 4, move to the UV category
-        elif request == 4:
-            uv()
+        elif menu_option == 4:
+            return air_quality_info['uvi'];
 
-        # If the user chose the index number 5, move to the Index Level category
-        elif request == 5:
-            index_level()
+        elif menu_option == 5:
+            index_level();
 
-        # If the user chose the index number 6, move to the Index Level Information Meaning category
-        elif request == 6:
-            index_info()
+        elif menu_option == 6:
+            index_info();
 
-        # If the user chose the index number 7, the program close
-        elif request == 7:
-            os.system("cls || clear")
-            print("Thanks for using our service.")
-            sys.exit(0)
+        elif menu_option == 7:
+            clear_console();
+            print("Thanks for using our service.");
+            exit(0);
 
-        # If the user chose the index number higher than 7 or less than 1, print an our of index error
-        elif request > 7 or request < 1:
-            print("Index number not available! Try another one.")
-            time.sleep(2)
+        elif menu_option > 7 or menu_option < 1:
+            print("Index number not available! Try another one.");
+            sleep(2);
 
-# Category that explain the informations about the air quality
 def index_info():
-    os.system("cls||clear")
+    """Explain the informations about the air quality"""
+    clear_console();
+
     print("Learn the meaning of ozone, particulate matter (PM) 10 and 2.5, and ultraviolet (UV) index information here.")
 
     # Block that explain what are the informations gathered in the program.
@@ -158,9 +161,10 @@ def index_info():
     print("Press enter to return to air quality information!")
     input()
     
-# Category with AQI, PMs and UV index level scale:
 def index_level():
-    os.system('cls||clear')
+    """Explain AQI, PMs and UV index level scale"""
+    
+    clear_console();
 
     # Ultraviolet Index Scale Meaning
     print("""
@@ -212,128 +216,64 @@ def index_level():
     print("Press enter to return to air quality information!")
     input()
 
-# Forecast informations about Ozone with 2 days old and 4 day predictions
-def oz():
-    os.system("cls||clear")
-    sum = 0
-    print("\nForecast:\n")
+def clear_console():
+    """Clear the console"""
 
-    # In a loop, gathering the information for the ozone category
-    for sum in range(0, len(ozone)):
-        for i in ozone[sum]['day']:
-            print(i, end='')
+    system("cls || clear");
 
-        print("\nThe average Ozone is ", end='')
-        for b in str(ozone[sum]['avg']):
-            print(b, end='')
-
-        print("\nThe max Ozone is ", end='')
-        for c in str(ozone[sum]['max']):
-            print(c, end='')
-
-        print("\nThe min Ozone is ", end='')
-        for d in str(ozone[sum]['avg']):
-            print(d, end='')
-        print("\n")
-        sum += 1
-
-    print("Press enter to return to air quality information!")
-    input()
-
-# Forecast informations about PM10 with 2 days old and 4 day predictions
-def pmten():
-    os.system("cls||clear")
-    sum = 0
-    print("Forecast:\n")
-
-    # In a loop, gathering the information for the PM10 category
-    for sum in range(0, len(pm_ten)):
-        for i in pm_ten[sum]['day']:
-            print(i, end='')
-
-        print("\nThe average PM10 is ", end='')
-        for b in str(pm_ten[sum]['avg']):
-            print(b, end='')
-
-        print("\nThe max PM10 is ", end='')
-        for c in str(pm_ten[sum]['max']):
-            print(c, end='')
-
-        print("\nThe min PM10 is ", end='')
-        for d in str(pm_ten[sum]['avg']):
-            print(d, end='')
-
-        print("\n")
-        sum += 1
-
-    print("Press enter to return to air quality information!")
-    input()
-
-# Forecast informations about PM2.5 with 2 days old and 4 day predictions
-def pmtwo():
-    os.system("cls||clear")
-    sum = 0
-    print("Forecast:\n")
-
-    # In a loop, gathering the information for the PM2.5 category
-    for sum in range(0, len(pm_twofive)):
-        for i in pm_twofive[sum]['day']:
-            print(i, end='')
-
-        print("\nThe average PM2.5 is ", end='')
-        for b in str(pm_twofive[sum]['avg']):
-            print(b, end='')
-
-        print("\nThe max PM2.5 is ", end='')
-        for c in str(pm_twofive[sum]['max']):
-            print(c, end='')
-
-        print("\nThe min PM2.5 is ", end='')
-        for d in str(pm_twofive[sum]['avg']):
-            print(d, end='')
-
-        print("\n")
-        sum += 1
-
-    print("Press enter to return to air quality information!")
-    input()
-
-# Forecast informations about UV Index with 2 days old and 4 day predictions
-def uv():
-    os.system("cls||clear")
-    sum = 0
-    print("Forecast:\n")
-
-    # In a loop, gathering the information for the UV category
-    for sum in range(0, len(uvi)):
-        for i in uvi[sum]['day']:
-            print(i, end='')
-
-        print("\nThe average UV Index is ", end='')
-        for b in str(uvi[sum]['avg']):
-            print(b, end='')
-
-        print("\nThe max UV Index is ", end='')
-        for c in str(uvi[sum]['max']):
-            print(c, end='')
-
-        print("\nThe min UV Index is ", end='')
-        for d in str(uvi[sum]['avg']):
-            print(d, end='')
-
-        print("\n")
-        sum += 1
-
-    print("Press enter to return to air quality information!")
-    input()
-
-# Start the program
-if __name__ == "__main__":
-    os.system("cls || clear")
     print("""
-    \t\t\t ________________________
-    \t\t\t|Welcome to AirQualityPy!|
+    \t\t\t _______________
+    \t\t\t| AirQualityPy! |
+    \t\t\t ---------------
     """)
-    data()
+
+def air_quality_menu(air_quality_info):
+    """Display Current Air Quality Informations"""
+
     while True:
-        air()
+        clear_console();
+
+        current_time      = air_quality_info['current_time'];
+        current_pollutant = air_quality_info['dominant_pollutant'];
+        current_aqi       = air_quality_info['current_aqi'];
+        current_pm25      = air_quality_info['current_pm25'];
+        current_city      = air_quality_info['current_city'];
+        
+        print(f"""
+        \tCurrent Date & Time         : {current_time}
+        \tCurrent Dominant Pollutant  : {current_pollutant.upper()}
+        \tCurrent Air Quality Index   : {current_aqi}
+        \tCurrent PM2.5 value         : {current_pm25}
+        \tAir Quality Station Location: {current_city}
+        """)
+        print(
+            "\t\t1.[PM10] / 2.[PM2.5] / 3.[Ozone] / 4.[UV Index]\n\t5.[Index Level Scale] / 6.[Data Reference information] / 7.[Quit]")
+        
+        menu_option = input("\nWhich information do you want to access? [Index #]\n>>");
+        aq_info     = fetch_forecast_air_quality(menu_option, air_quality_info);
+        display_air_quality_element(aq_info) if type(aq_info) is list else aq_info;
+
+def main():
+    """Main function of the program"""
+    
+    clear_console();
+
+    key_api   = ""; # --- your key API here --- # 
+    BASE_URL  = "https://api.waqi.info/feed/geo:"
+
+    zip_code  = input("Please, provide your zip code: ");
+    location  = get_location(zip_code);
+    time_zone = get_timezone(zip_code);
+    api_data  = get_api_data(BASE_URL, location, key_api);
+
+    if api_data:
+        data = api_data['data']
+        air_quality_info = get_air_quality_info(time_zone, data);
+        air_quality_menu(air_quality_info);
+
+    else:
+        print("Air Quality Information No Found!");
+
+
+if __name__ == "__main__":
+    main();
